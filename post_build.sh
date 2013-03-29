897456aaf54e3ab5959141f804d904f4e8f8b211
Wesley Hales <wesleyhales@gmail.com>
2013-03-29 06:10:56 -0400

    more s ettings

--- post_build.sh
+++ post_build.sh
@@ -10,5 +10,5 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
     git checkout -B gh-pages
     git add -f reports/.
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-    git push https://$(GH_TOKEN)@github.com/${REPO} gh-pages > /dev/null
+    git push https://${GH_TOKEN}@github.com/${REPO} gh-pages > /dev/null
 fi
