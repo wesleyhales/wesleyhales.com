842690fd9b4efc43e159ba56a23f5698eef4d0ae
Wesley Hales <wesleyhales@gmail.com>
2013-03-29 06:14:15 -0400

    almost there

--- post_build.sh
+++ post_build.sh
@@ -6,8 +6,8 @@ git config --global user.email "wesleyhales@gmail.com"
 git config --global user.name "Travis"
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    git branch -D gh-pages
-    git checkout -B gh-pages
+    git checkout gh-pages
+    git pull gh-pages
     git add -f reports/.
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
     git push https://${GH_TOKEN}@github.com/${REPO} gh-pages > /dev/null
