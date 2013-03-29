4fe693494b00ca1d19baab3227c34935e2c7f65c
Wesley Hales <wesleyhales@gmail.com>
2013-03-29 06:03:03 -0400

    fix branch name

--- post_build.sh
+++ post_build.sh
@@ -1,6 +1,6 @@
 export REPO="$(pwd | sed s,^/home/travis/build/,,g)"
 echo -e "Current Repo:$REPO --- Travis Branch:$TRAVIS_BRANCH"
-if [ "$TRAVIS_BRANCH" == "travis" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ]; then
     git branch -D gh-pages
     git checkout -B gh-pages
     git add -f reports/.
